e6edb349327265a2c9d46d31665bdd62e03b8f74
PalaceSoftware <123653137+PalaceSoftware@users.noreply.github.com>
2023-10-20 14:50:23 +0200

    user interaction removed
    * user don't have to allow the installation of dependencies anymore

--- install-timesync.sh
+++ install-timesync.sh
@@ -30,7 +30,7 @@ else
    exit 2
 fi
 echo "Dependencies will be downloaded..."
-apt install python3 python3-tk pkexec unzip wget sed
+apt install python3 python3-tk pkexec unzip wget sed -y
 echo "Dependencies downloaded."
 echo
 echo "Prepairing ressources archive..."
