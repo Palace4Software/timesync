8f28a8dde67b992f7533c6a4225aaf355cc35459
Palace4Software <123653137+Palace4Software@users.noreply.github.com>
2023-10-30 22:01:22 +0100

    check for firststart implemented
    - check for firststart implemented
    - better documentation
    [see changelog for more details]

--- languagesetup.py
+++ languagesetup.py
@@ -2,25 +2,50 @@
 ### Setup language for timesync ###
 
 def main():
+    print("Initalize 'Change lanuguage' subwindow...")
     import tkinter as gui
     from tkinter import ttk
+    from tkinter import messagebox
     import os
     import platformdirs
 
     def seteng():
-        config_path = platformdirs.user_config_dir("timesync")
         with open(config_path + "/language.cfg", "w") as langcfg:
             print("EN", file=langcfg)
             print("", file=langcfg)
+        print("Language set to English.")
         GUI.destroy()
+        if firststart == True:
+            messagebox.showwarning("Language changed", "The language is now set to English.")
+        else:
+            messagebox.showwarning("Language changed", "You may have to restart the application to apply the changes.")
         
     def setger():
-        config_path = platformdirs.user_config_dir("timesync")
         with open(config_path + "/language.cfg", "w") as langcfg:
             print("DE", file=langcfg)
             print("", file=langcfg)
+        print("Language set to German.")
         GUI.destroy()
-        
+        if firststart == True:
+            messagebox.showwarning("Sprache geändert", "Sie müssen die Anwendung schließen und neu öffnen, damit die Sprache übernommen werden kann.")
+        else:
+            messagebox.showwarning("Sprache geändert", "Möglicherweise müssen Sie die Anwendung neu starten, um die Änderungen zu übernehmen.")
+    
+    config_path = platformdirs.user_config_dir("timesync")
+    checkfirststart = os.path.isfile(config_path + "/firststart.cfg")
+    if checkfirststart == True:
+        firststart = False # no firststart (because file exist)
+        with open(config_path + "/firststart.cfg", "w") as frststrt:
+            print("False", file=frststrt)
+            print("# DO NOT CHANGE OR DELETE THIS FILE", file=frststrt)
+            print("", file=frststrt)
+    else:
+        firststart = True # firststart (because file do not exist)
+        with open(config_path + "/firststart.cfg", "w") as frststrt:
+            print("True", file=frststrt)
+            print("# DO NOT CHANGE OR DELETE THIS FILE", file=frststrt)
+            print("", file=frststrt)
+    
     GUI = gui.Tk()
     GUI.title("Choose language")
     GUI.geometry("187x100")
@@ -34,7 +59,8 @@ def main():
     germanbutton.pack()
     
     spaceholder2 = gui.Label(GUI).pack(expand=True, fill="x")
-
+    
+    print("Initialisation of subwindow finished.")
     GUI.mainloop()
 
 # main() # uncomment this line for debugging
